a3a4d32b0b5e647d86f02ac04bf7e72172b0a652
wangui.njoka <wangui.njoka@gmail.com>
2020-02-27 23:54:11 +0300

    learning angular-material

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule,
+         MatButtonToggleModule} from '@angular/material';
+
+const MaterialComponents = [
+  MatButtonModule,
+  MatButtonToggleModule
+];
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports: [MaterialComponents]
+})
+export class MaterialModule { }
